fc49bbeaed4e4fb87bf2fb42733209d80f83d085
gvaldiv <gvaldiv@us.ibm.com>
2018-03-05 15:08:07 -0500

    add func to fix basic app

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Comment from './components/Comment';
 
 export default class Hello extends React.Component {
-  render() {
+  render (){
+    const comments = this._getComments();
     return (
-      <div>
-        Hello from react
+      <div className="comment-box">
+        <h3>Comments</h3>
+        <h4 className="comment-count">{comments.length} comments</h4>
+        <div className="comment-list">
+          {comments}
+        </div>
       </div>
-    );
+    )
+  }
+  _getComments(){
+    const commentList = [
+      {id:1, author:'Morgan McCircuit', body:'Great picture!'},
+      {id:2, author:'Bending Bender', body:'Excellent Stuff'}
+    ];
+    return commentList.map((comment) =>{
+      return (
+        <Comment
+          author={comment.author} body={comment.body} key={comment.id}/>
+      );
+    });
   }
 }
 
